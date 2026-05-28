f8438dfbfc702a65b9ec032904edecff4f474e01
karchx <kenethra.74@gmail.com>
2026-05-28 15:07:50 -0600

    feat: add web server

--- stellar_core/src/stellar_core_app.erl
+++ stellar_core/src/stellar_core_app.erl
@@ -4,12 +4,20 @@
 %%%-------------------------------------------------------------------
 
 -module(stellar_core_app).
-
 -behaviour(application).
 
 -export([start/2, stop/1]).
+-define(COWBOY_PORT, 8080).
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        { '_', [
+            {"/", web_server_handler, #{path => <<"home">>}},
+            {"/random", web_server_handler, #{path => <<"random">>}},
+            {"/time", web_server_handler, #{path => <<"time">>}}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(http_listener, [{port, ?COWBOY_PORT}], #{env => #{dispatch => Dispatch}}),
     stellar_core_sup:start_link().
 
 stop(_State) ->
